8c852b1f67a65ead227672343e46286034f44b0e
Akshat240 <lcs2020036@iiitl.ac.in>
2024-02-19 21:50:11 +0530

    Frontend commit1

---
+++ frontend/src/pages/home/sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+    <div>
+        Search Input
+      {/* Search Input */}
+    </div>
+    <div className='divider'></div>
+    <div>
+        {/* Chat List */}
+        To be done
+    
+    
+    </div>
+    </>
+  )
+}
+
+export default Sidebar;
